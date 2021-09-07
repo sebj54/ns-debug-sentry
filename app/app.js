6bc9bc9be7139132a97deacec929e5f0423cb2fe
Sébastien JEAN <contact@sebjean.com>
2021-09-07 10:03:02 +0200

    ➕  Add  @nativescript-community/sentry

--- app/app.js
+++ app/app.js
@@ -1,6 +1,29 @@
 import Vue from 'nativescript-vue'
+import { isAndroid } from '@nativescript/core'
+import * as Sentry from '@nativescript-community/sentry'
+import { getBuildNumber, getVersionName } from 'nativescript-extendedinfo';
 
 import Home from './components/Home'
+
+const SENTRY_DSN = 'https://62e49d5d671847fd9cae08e00fd9e91d@o124048.ingest.sentry.io/5948658'
+
+Promise.all([getVersionName(), getBuildNumber()])
+    .then(([versionName, buildNumber]) => {
+        Sentry.init({
+            dsn: SENTRY_DSN,
+            // debug: true,
+            appPrefix: 'app:///',
+            release: versionName,
+            dist: `${buildNumber}.${isAndroid ? 'android' : 'ios'}`,
+        })
+    })
+    .catch((error) => {
+        if (error && error.stack) {
+            console.error(error.stack)
+        } else {
+            console.error(error)
+        }
+    })
 
 new Vue({
   render: (h) => h('frame', [h(Home)]),
--- app/components/Home.vue
+++ app/components/Home.vue
@@ -4,25 +4,58 @@
             <Label text="Home"/>
         </ActionBar>
 
-        <GridLayout>
+        <FlexboxLayout
+            flexDirection="column"
+            justifyContent="center"
+            alignItems="center"
+            textAlign="center"
+        >
             <Label class="info">
                 <FormattedString>
                     <Span class="fas" text.decode="&#xf135; "/>
-                    <Span :text="message"/>
+                    <Span text="Blank {N}-Vue app"/>
                 </FormattedString>
             </Label>
-        </GridLayout>
+
+            <Button
+                text="Throw exception"
+                @tap="throws"
+            />
+
+            <Button
+                text="Capture exception"
+                @tap="capture"
+            />
+
+            <Label class="info" textWrap="true">
+                <FormattedString>
+                    <Span text="Last event ID: "/>
+                    <Span :text="lastEventId || 'none'"/>
+                </FormattedString>
+            </Label>
+        </FlexboxLayout>
     </Page>
 </template>
 
 <script>
-  export default {
-    computed: {
-      message() {
-        return "Blank {N}-Vue app";
-      }
-    }
-  };
+import * as Sentry from '@nativescript-community/sentry'
+
+export default {
+    data() {
+        return {
+            lastEventId: null,
+        }
+    },
+    methods: {
+        capture() {
+            const error = new Error('A blank error thrown by a button, captured by Sentry')
+            this.lastEventId = Sentry.captureException(error)
+        },
+        throws() {
+            throw new Error('A blank error thrown by a button, not captured')
+        },
+    },
+};
 </script>
 
 <style scoped lang="scss">
